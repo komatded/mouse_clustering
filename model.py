f7f4972cc8a563ff87a7643803b4f491bba116e7
Matvey <mkolbasov@cybertonica.com>
2021-01-04 17:55:01 +0300

    upd

--- model.py
+++ model.py
@@ -19,8 +19,8 @@ def bpr_triplet_loss(embedding_anchor, embedding_positive, embedding_negative):
 
 def create_base_model(input_shape, embedding_size):
     input_layer = Input(shape=input_shape)
-    input_layer = BatchNormalization()(input_layer)
-    x = LSTM(100, return_sequences=False)(input_layer)  # dropout=0.1, recurrent_dropout=0.1
+    x = BatchNormalization()(input_layer)
+    x = LSTM(100, return_sequences=False)(x)  # dropout=0.1, recurrent_dropout=0.1
     x = Dense(embedding_size, activation='relu')(x)
     base_network = Model(inputs=input_layer, outputs=x)
     return base_network
